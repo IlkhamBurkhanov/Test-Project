9e8351c49970440604a6ce93add7c7aea00cb7bf
IlkhamBurkhonov <2010053.ilkhomjon.burkhonov@gmail.com>
2024-10-03 16:23:03 +0500

    margin y

--- src/components/MessageText/MessageText.jsx
+++ src/components/MessageText/MessageText.jsx
@@ -2,7 +2,7 @@ import { Box, Typography } from "@mui/material"
 
 const MessageText = ({textBold, text})=> {
     return(
-       <Box sx={{margin:"32px", paddingX:"24px", paddingY:"20px", backgroundColor:"#F5FFFB", border: "1px solid #2FB1EA", borderRadius: "12px", fontStyle: 'italic'}}>
+       <Box sx={{margin:"24px 32px", paddingX:"24px", paddingY:"20px", backgroundColor:"#F5FFFB", border: "1px solid #2FB1EA", borderRadius: "12px", fontStyle: 'italic'}}>
        <Typography variant="h6" sx={{fontSize:"15px"}}>
             <Box component="span" sx={{ fontWeight: 'bold', textTransform: 'uppercase'  }}>
                 {textBold}
